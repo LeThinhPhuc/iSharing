d828f1355b1c8c873d9ef5882d0d4621968af914
LeThinhPhuc <phuclethinh70@gmail.com>
2024-05-17 10:28:00 +0700

    phuc2

--- iSharing2/app/src/main/java/com/example/isharing/MainActivity.java
+++ iSharing2/app/src/main/java/com/example/isharing/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.isharing;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +14,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,6 +22,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
@@ -30,9 +32,12 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.bumptech.glide.Glide;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,11 +60,48 @@ public class MainActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 auth = FirebaseAuth.getInstance();
+                                String uid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+                                db = FirebaseFirestore.getInstance();
+
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("Name", auth.getCurrentUser().getDisplayName());
+                                user.put("Email", auth.getCurrentUser().getEmail());
+                                user.put("Photo", Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl());
+                                db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Toast.makeText(MainActivity.this, "luu thanh cong goi", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+                                // Kiểm tra xem tài liệu người dùng đã tồn tại hay chưa
+                                db.collection("users").document(uid).get()
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+                                            @Override
+                                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                Toast.makeText(MainActivity.this, "cb ne ne them", Toast.LENGTH_SHORT).show();
+
+                                                if (documentSnapshot.exists()) {
+                                                    // Người dùng đã tồn tại, có thể cập nhật thông tin nếu cần thiết
+                                                    Toast.makeText(MainActivity.this, "cb update", Toast.LENGTH_SHORT).show();
+
+                                                    updateUserInfo(uid, auth.getCurrentUser().getDisplayName(), auth.getCurrentUser().getEmail(), Objects.requireNonNull(auth.getCurrentUser().getPhotoUrl()));
+                                                } else {
+                                                    // Người dùng chưa tồn tại, lưu thông tin mới
+                                Toast.makeText(MainActivity.this, "cb them", Toast.LENGTH_SHORT).show();
+
+                                                    saveUserInfo(uid, auth.getCurrentUser().getDisplayName(), auth.getCurrentUser().getEmail(), Objects.requireNonNull(auth.getCurrentUser().getPhotoUrl()));
+                                                }
+                                            }
+                                        });
+
+                                // Cập nhật giao diện người dùng
                                 Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
                                 name.setText(auth.getCurrentUser().getDisplayName());
                                 mail.setText(auth.getCurrentUser().getEmail());
-                                Toast.makeText(MainActivity.this, "Signed in success", Toast.LENGTH_SHORT).show();
-                            }else{
+//                                Toast.makeText(MainActivity.this, "Signed in success", Toast.LENGTH_SHORT).show();
+                            } else {
                                 Toast.makeText(MainActivity.this, "Fail "+ task.getException(), Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -70,18 +112,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
         imageView = findViewById(R.id.profileImage);
         name = findViewById(R.id.nameTV);
         mail = findViewById(R.id.mailTV);
         GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.client_id)).requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(MainActivity.this, options);
-
-        auth = FirebaseAuth.getInstance();
 
         SignInButton signInButton = findViewById(R.id.signIn);
         signInButton.setOnClickListener(new View.OnClickListener() {
@@ -119,33 +163,55 @@ public class MainActivity extends AppCompatActivity {
             name.setText(auth.getCurrentUser().getDisplayName());
             mail.setText(auth.getCurrentUser().getEmail());
         }
-
-
-//        Map<String, Object> user = new HashMap<>();
-//        user.put("first", "Alan");
-//        user.put("middle", "Mathison");
-//        user.put("last", "Turing");
-//        user.put("born", 1912);
-//        db= FirebaseFirestore.getInstance();
-
-
-
-//        db.collection("users")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//
-//                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
     }
 
+    // Hàm để lưu thông tin người dùng vào Firestore lần đầu tiên
+    private void saveUserInfo(String uid, String displayName, String email, Uri photoUrl) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("Name", displayName);
+        user.put("Email", email);
+        user.put("Photo", photoUrl.toString());
+        // Thêm các thông tin khác của người dùng nếu cần
 
+        db.collection("users").document(uid)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Thành công, hiển thị thông tin của người dùng và các hành động khác
+                        Toast.makeText(MainActivity.this, "Save successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Lỗi khi lưu thông tin người dùng vào Firestore
+                        Toast.makeText(MainActivity.this, "Save fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    // Hàm để cập nhật thông tin người dùng trong Firestore
+    private void updateUserInfo(String uid, String displayName, String email, Uri photoUrl) {
+        DocumentReference userRef = db.collection("users").document(uid);
+
+        // Cập nhật thông tin người dùng trong tài liệu
+        userRef.update("Name", displayName,
+                        "Email", email,
+                        "Photo", photoUrl.toString())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Thành công, hiển thị thông tin của người dùng và các hành động khác
+                        Toast.makeText(MainActivity.this, "Update successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Lỗi khi cập nhật thông tin người dùng trong Firestore
+                        Toast.makeText(MainActivity.this, "Update Fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
